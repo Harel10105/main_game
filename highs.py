be41c11c8c1cebaf7260f335817b8f9aafd6707d
tug35814 <malul@som.bgu.ac.il>
2022-04-13 16:57:51 +0300

    main hub changes

---
+++ highs.py
@@ -0,0 +1,7 @@
+import pygame
+from helpers_and_functions.constants_files.constants_menu import *
+
+def display_high_scores():
+    pygame.init()
+    screen = pygame.display.set_mode((WIDTH, HEIGHT))
+
